c270e4978081cd2726269ffeea7bb10f7d642930
Ainunnajwaa <143976794+Ainunnajwaa@users.noreply.github.com>
2025-07-19 19:42:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,5 +25,12 @@ function playMusic() {
     alert("Gagal memutar musik. Silakan klik lagi.");
     console.error(error);
   });
+  function playMusic() {
+  const videoID = "D-VytLhH-KE";
+  const iframe = document.getElementById("ytMusic");
+  iframe.src = `https://www.youtube.com/embed/${videoID}?autoplay=1&loop=1&playlist=${videoID}`;
+  document.getElementById("musicBtn").style.display = "none";
+}
+
 }
 
